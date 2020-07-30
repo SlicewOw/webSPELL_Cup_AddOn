d25f7aff5784687b7cae0e8be54be48c5bc846e9
slicewuff <me@slicewuff.de>
2020-07-30 20:54:34 +0200

    Add first enums

---
+++ src/cup/enum/cup_enums.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace myrisk\Cup\Enum;
+
+abstract class CupEnums
+{
+
+    /**
+     * Cup status
+     */
+    const CUP_STATUS_REGISTRATION = 1;
+    const CUP_STATUS_CHECKIN = 2;
+    const CUP_STATUS_RUNNING = 3;
+    const CUP_STATUS_FINISHED = 4;
+
+}
