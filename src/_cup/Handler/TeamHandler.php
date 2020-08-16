f2ef4d40ea11241808529321f5994bf666f25ae1
slicewuff <me@slicewuff.de>
2020-08-16 15:14:01 +0200

    Rename folder temporarily

---
+++ src/_cup/Handler/TeamHandler.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace myrisk\Cup\Handler;
+
+use Respect\Validation\Validator;
+
+use webspell_ng\WebSpellDatabaseConnection;
+
+use myrisk\Cup\Team;
+
+class TeamHandler {
+
+    public static function getTeamByTeamId(int $team_id): Team
+    {
+
+        if (!Validator::numericVal()->min(1)->validate($team_id)) {
+            throw new \InvalidArgumentException('team_id_value_is_invalid');
+        }
+
+        $queryBuilder = WebSpellDatabaseConnection::getDatabaseConnection()->createQueryBuilder();
+        $queryBuilder
+            ->select('*')
+            ->from(WebSpellDatabaseConnection::getTablePrefix() . 'cups_teams')
+            ->where('teamID = ?')
+            ->setParameter(0, $team_id);
+
+        $team_query = $queryBuilder->execute();
+        $team_result = $team_query->fetch();
+
+        $team = new Team();
+        $team->setTeamId($team_result['teamID']);
+        $team->setName($team_result['name']);
+        $team->setTag($team_result['tag']);
+        $team->setCountry($team_result['country']);
+        $team->setHomepage($team_result['hp']);
+
+        return $team;
+
+    }
+
+}
---
+++ src/_cup/UserParticipant.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace myrisk\Cup;
+
+use webspell_ng\User;
+
+class UserParticipant extends Participant {
+
+    /** @var User $user */
+    private $user;
+
+    public function setUser(User $user): void
+    {
+        $this->user = $user;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+}
