48066a9a21faf541688b5457c635d27fe766035e
slicewuff <me@slicewuff.de>
2020-11-08 14:33:01 +0100

    Use DateUtils in class TeamMemberHandler

--- src/Cup/Handler/TeamMemberHandler.php
+++ src/Cup/Handler/TeamMemberHandler.php
@@ -6,6 +6,7 @@ use Respect\Validation\Validator;
 
 use webspell_ng\WebSpellDatabaseConnection;
 use webspell_ng\Handler\UserHandler;
+use webspell_ng\Utils\DateUtils;
 
 use myrisk\Cup\Team;
 use myrisk\Cup\TeamMember;
@@ -40,10 +41,22 @@ class TeamMemberHandler {
             UserHandler::getUserByUserId($member_result['userID'])
         );
         $member->setPosition($member_result['position']);
-        $member->setJoinDate($member_result['join_date']);
-        $member->setLeftDate($member_result['left_date']);
-        $member->setKickId($member_result['kickID']);
+        $member->setJoinDate(
+            DateUtils::getDateTimeByMktimeValue($member_result['join_date'])
+        );
         $member->setIsActive($member_result['active']);
+
+        $left_date_value = $member_result['left_date'];
+        if ($left_date_value > 0) {
+            $member->setLeftDate(
+                DateUtils::getDateTimeByMktimeValue($member_result['left_date'])
+            );
+        }
+
+        $kick_id = $member_result['kickID'];
+        if ($kick_id > 0) {
+            $member->setKickId($kick_id);
+        }
 
         return $member;
 
