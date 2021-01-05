9c229b0d3ecf83be09093a1656a8031a0dade363
Sebastian Schulz <sebastian.schulz@plath.de>
2021-01-05 12:32:25 +0100

    Remove unused files

--- tests/resources/travis.sql
+++
@@ -1,7 +0,0 @@
-# Create Testuser
-CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
-GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'dev'@'localhost';
-
-# Create DB
-CREATE DATABASE IF NOT EXISTS `webspell_ng` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `webspell_ng`;
